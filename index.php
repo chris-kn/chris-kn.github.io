6f90984c8d032f98ee2e9bf5bb1110f439da4b6a
Christopher Knight <knight.christo@gmail.com>
2016-11-23 16:36:49 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+header("Status: 301 Moved Permanently", false, 301);
+header('Location: <a href="http://christopherknight.audio" rel="nofollow">http://christopherknight.audio</a>');
+exit();
+?>
